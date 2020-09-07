c601f90301f4ba94596929452c68b9309bdbf5ee
IvanKnop <ivan.knopoff@gmail.com>
2020-09-07 19:31:37 -0300

    importado widget avatar

--- codigo/interfazInicial.py
+++ codigo/interfazInicial.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg 
 import os
+import codigo.visorImagenes as vImg
 
 def actualizar_columnas(ventana, *columna):
     '''Vuelve visible la columna que recibe como parámetro e invisible
@@ -24,23 +25,20 @@ def inicio(img):
     ]
     return layout
 
-def jugar_interfaz(img_juego):
+def jugar_interfaz(imagenes):
     layout = [
-        [sg.Button(),sg.Image(filename=img_juego),sg.Button()],
-        [sg.Button(),sg.Button(),sg.Button(),sg.Button(),sg.Button()],
-        [sg.Button(border_width=0,button_text='VOLVER', button_color=('#FF0','#000'), size=(10, 2), font=('Impact', 20), key='volver')],
+         [sg.Column(imagenes.getAvatarLayout(),visible=False,key='colAvatar')],
+        [sg.Button(border_width=0,button_text='VOLVER', button_color=('#FF0','#000'), size=(50, 2), font=('Impact', 20), key='volver')],
     ]
     return layout
 
-def interfaz_principal(titulo, img_juego):
+def interfaz_principal(titulo, img_juego,imagenes):
     colInicial = inicio(titulo)
-    colJugar = jugar_interfaz(img_juego)
-    #Al ejecutarse, sólo será visible la columna inicial
     layout = [
-        [sg.Image(filename=titulo)],
         [sg.Column(colInicial,justification='center',element_justification='center',key= 'colInicial'),
-         sg.Column(colJugar,visible=False,justification='center',element_justification='center',key='colJugar'),
-        ],
+
+         sg.Column(jugar_interfaz(imagenes),visible=False,justification='center',element_justification='center',key='colJugar'),
+        ]
     ]
     return layout
 
@@ -48,15 +46,19 @@ def juego():
     ANCHO = 400
     ALTO = 600
     titulo = os.path.join ('media','titulo.png')
-    img_juego = os.path.join('media','imagenes','leon2.png')
-    ventana = sg.Window('Palabreando', interfaz_principal(titulo,img_juego), size = (ANCHO,ALTO),background_color='#DDD' ,element_justification='center',no_titlebar=False)
+    img_juego = os.path.join('media','imagenes','leon.png')
+    directorio = os.path.join('media','imagenes','')
+    imagenes = vImg.Visor(directorio)
+    ventana = sg.Window('Palabreando', interfaz_principal(titulo,img_juego,imagenes), size = (ANCHO,ALTO),background_color='#DDD' ,element_justification='center',no_titlebar=False)
     ventana.Finalize()
     while True:
         event, values = ventana.read()
         if (event in (None, 'salir')):
             break
         if (event == 'jugar'):
-            actualizar_columnas(ventana, 'colJugar')
+            actualizar_columnas(ventana, 'colJugar','colAvatar')
         if (event == 'volver'):
             actualizar_columnas(ventana,'colInicial')
+        elif event in ('<<<', '>>>'):
+            avatarSelec = imagenes.controles(event, ventana.FindElement('avatarVisor'))
     ventana.Close()       
